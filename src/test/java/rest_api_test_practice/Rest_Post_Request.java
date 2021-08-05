f8027ca46c0499dfac269a434e1130dab850ba94
GOKUL <agogul.1991@gmail.com>
2021-08-05 07:01:09 +0530

    first commit

--- src/test/java/rest_api_test_practice/Rest_Post_Request.java
+++ src/test/java/rest_api_test_practice/Rest_Post_Request.java
@@ -34,4 +34,46 @@ public class Rest_Post_Request {
         System.out.println(data);
         System.out.println(data1);
     }
+    @Test
+    public void post(){
+        RestAssured.baseURI = "https://petstore.swagger.io";
+        RequestSpecification http_request = RestAssured.given();
+
+
+        String str = """
+                {
+                  "id": 0,
+                  "category": {
+                    "id": 10,
+                    "name": "gokul"
+                  },
+                  "name": "doggie",
+                  "photoUrls": [
+                    "string"
+                  ],
+                  "tags": [
+                    {
+                      "id": 0,
+                      "name": "tag_name"
+                    }
+                  ],
+                  "status": "available"
+                }""";
+
+
+        http_request.header("Content-Type","application/json");//must use this while post action
+        http_request.request().body(str);
+
+        Response response = http_request.request(Method.POST, "/v2/pet");
+        Object data = response.jsonPath().get("name");
+        Object data1 = response.jsonPath().get("category.name");
+        Object data2 = response.jsonPath().get("tags[0].name");
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getStatusLine());
+        System.out.println(data);
+        System.out.println(data1);
+        System.out.println(data2);
+
+    }
 }
